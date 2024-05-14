b72427a02dc6c4b92a526c0b978cc00d8634176e
MauricioGalaz <154044574+MauricioGalaz@users.noreply.github.com>
2024-05-14 00:22:40 -0400

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page'; // Importa el componente HomePage
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    component: HomePage, // Asigna el componente HomePage a la ruta 'home'
   },
   {
-    path: '',
+    path: '', // Define una redirección por defecto
     redirectTo: 'home',
     pathMatch: 'full',
   },
